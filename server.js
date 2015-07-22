e14263cac235eff8019be80dc0a01a35aae8a6d3
Adam Thede <adam@thedetech.com>
2015-07-22 16:42:21 -0500

    first commit on fork and to branch

--- server.js
+++ server.js
@@ -2,4 +2,22 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
+var express = require('express');
+var app = express();
+
 var jsonData = {count: 12, message: 'hey'};
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html', function(err){
+		res.status(500).send(err);
+	});
+});
+
+app.get('/data', function(req, res){
+	res.json(jsonData);
+});
+
+var port = 3000;
+app.listen(port, function(){
+	console.log('listening on http://localhost:' + port);
+});
