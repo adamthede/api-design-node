bf56ac7355b96e0c71c57ce1bbad8ece184d824c
Adam Thede <adam@thedetech.com>
2015-07-22 17:11:12 -0500

    using routers and subrouters

--- server/lions.js
+++ server/lions.js
@@ -1,58 +1,68 @@
+var _ = require('lodash');
 var lionRouter = require('express').Router();
 
 var lions = [];
 var id = 0;
 
 var updateId = function(req, res, next) {
-  if (!req.body.id) {
-    id++;
-    req.body.id = id + '';
-  }
-  next();
+	if (!req.body.id) {
+		id++;
+		req.body.id = id + '';
+	}
+	next();
 };
 
 lionRouter.param('id', function(req, res, next, id) {
-  var todo = _.find(todos, {id: id});
+	var lion = _.find(lions, {
+		id: id
+	});
 
-  if (todo) {
-    req.todo = todo;
-    next();
-  } else {
-    res.send();
-  }
+	if (lion) {
+		req.lion = lion;
+		next();
+	} else {
+		res.send();
+	}
 });
 
-lionRouter.get('/', function(req, res){
-  res.json(lions);
+lionRouter.get('/', function(req, res) {
+	res.json(lions);
 });
 
-lionRouter.get('/:id', function(req, res){
-  var lion = req.todo;
-  res.json(lion || {});
+lionRouter.get('/:id', function(req, res) {
+	var lion = req.lion;
+	res.json(lion || {});
 });
 
 lionRouter.post('/', updateId, function(req, res) {
-  var lion = req.body;
-
-  lions.push(lion);
-
-  res.json(lion);
+	var lion = req.body;
+	lions.push(lion);
+	res.json(lion);
 });
 
-
 lionRouter.put('/:id', function(req, res) {
-  var update = req.body;
-  if (update.id) {
-    delete update.id
-  }
+	var update = req.body;
+	if (update.id) {
+		delete update.id;
+	}
 
-  var lion = _.findIndex(lions, {id: req.params.id});
-  if (!lions[lion]) {
-    res.send();
-  } else {
-    var updatedLion = _.assign(lions[lion], update);
-    res.json(updatedLion);
-  }
+	var lion = _.findIndex(lions, {
+		id: req.params.id
+	});
+	if (!lions[lion]) {
+		res.send();
+	} else {
+		var updatedLion = _.assign(lions[lion], update);
+		res.json(updatedLion);
+	}
+});
+
+lionRouter.delete('/:id', function(req, res) {
+	var lion = _.findIndex(lions, {
+		id: req.params.id
+	});
+	lions.splice(lion, 1);
+	res.json(req.lion);
 });
 
 module.exports = lionRouter;
--- server/server.js
+++ server/server.js
@@ -9,20 +9,23 @@ var morgan = require('morgan');
 var lionRouter = require('./lions');
 var tigerRouter = require('./tigers');
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.use(express.static('client'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use(bodyParser.json());
 // this is called mounting. when ever a req comes in for
 // '/lion' we want to use this router
 app.use('/lions', lionRouter);
+app.use('/tigers', tigerRouter);
 
 app.use(function(err, req, res, next) {
-  if (err) {
-    res.status(500).send(error);
-  }
+	if (err) {
+		console.log(err.message);
+		res.status(500).send(err);
+	}
 });
-
 
 app.listen(3000);
 console.log('on port 3000');
