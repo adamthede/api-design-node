87a63d03941e440033970e7dd45350668a8d4085
Adam Thede <adam@thedetech.com>
2015-07-23 16:41:28 -0500

    setting up routes in controllers

--- server/api/category/categoryController.js
+++ server/api/category/categoryController.js
@@ -2,51 +2,69 @@ var Category = require('./categoryModel');
 var _ = require('lodash');
 
 exports.params = function(req, res, next, id) {
-  // use the id and attach the category to req
+	// use the id and attach the category to req
+	Category.findById(id)
+		.then(function(category) {
+			if (!category) {
+				next(new Error('No category with that id'));
+			} else {
+				req.category = category;
+				next();
+			}
+		}, function(err) {
+			next(err);
+		});
 };
 
 exports.get = function(req, res, next) {
-
+	Category.find({})
+		.then(function(categories){
+            if(!categories) {
+                next(new Error('No categories in the db'));
+            } else {
+                res.json(categories);
+            }
+        }, function(err) {
+            next(err);
+        });
 };
 
 exports.getOne = function(req, res, next) {
-  var category = req.category;
-  res.json(category);
+	var category = req.category;
+	res.json(category);
 };
 
 exports.put = function(req, res, next) {
-  var category = req.category;
+	var category = req.category;
+	var update = req.body;
+	_.merge(category, update);
 
-  var update = req.body;
-
-  _.merge(category, update);
-
-  category.save(function(err, saved) {
-    if (err) {
-      next(err);
-    } else {
-      res.json(saved);
-    }
-  })
+	category.save(function(err, saved) {
+		if (err) {
+			next(err);
+		} else {
+			res.json(saved);
+		}
+	});
 };
 
 exports.post = function(req, res, next) {
-  var newcategory = req.body;
+	var newcategory = req.body;
 
-  Category.create(newcategory)
-    .then(function(category) {
-      res.json(category);
-    }, function(err) {
-      next(err);
-    });
+	Category.create(newcategory)
+		.then(function(category) {
+			res.json(category);
+		}, function(err) {
+			next(err);
+		});
 };
 
 exports.delete = function(req, res, next) {
-  req.category.remove(function(err, removed) {
-    if (err) {
-      next(err);
-    } else {
-      res.json(removed);
-    }
-  });
+	req.category.remove(function(err, removed) {
+		if (err) {
+			next(err);
+		} else {
+			res.json(removed);
+		}
+	});
 };
--- server/api/post/postController.js
+++ server/api/post/postController.js
@@ -2,61 +2,68 @@ var Post = require('./postModel');
 var _ = require('lodash');
 
 exports.params = function(req, res, next, id) {
-  Post.findById(id)
-    .then(function(post) {
-      if (!post) {
-        next(new Error('No post with that id'));
-      } else {
-        req.post = post;
-        next();
-      }
-    }, function(err) {
-      next(err);
-    });
+	Post.findById(id)
+		.then(function(post) {
+			if (!post) {
+				next(new Error('No post with that id'));
+			} else {
+				req.post = post;
+				next();
+			}
+		}, function(err) {
+			next(err);
+		});
 };
 
 exports.get = function(req, res, next) {
-  // need to populate here
+	Post.find({})
+        .then(function(posts){
+            if(!posts) {
+                next(new Error('No posts!'));
+            } else {
+                res.json(posts);
+            }
+        }, function(err) {
+            next(err);
+        });
 };
 
 exports.getOne = function(req, res, next) {
-  var post = req.post;
-  res.json(post);
+	var post = req.post;
+	res.json(post);
 };
 
 exports.put = function(req, res, next) {
-  var post = req.post;
+	var post = req.post;
+	var update = req.body;
+	_.merge(post, update);
 
-  var update = req.body;
-
-  _.merge(post, update);
-
-  post.save(function(err, saved) {
-    if (err) {
-      next(err);
-    } else {
-      res.json(saved);
-    }
-  })
+	post.save(function(err, saved) {
+		if (err) {
+			next(err);
+		} else {
+			res.json(saved);
+		}
+	});
 };
 
 exports.post = function(req, res, next) {
-  var newpost = req.body;
+	var newpost = req.body;
 
-  Post.create(newpost)
-    .then(function(post) {
-      res.json(post);
-    }, function(err) {
-      next(err);
-    });
+	Post.create(newpost)
+		.then(function(post) {
+			res.json(post);
+		}, function(err) {
+			next(err);
+		});
 };
 
 exports.delete = function(req, res, next) {
-  req.post.remove(function(err, removed) {
-    if (err) {
-      next(err);
-    } else {
-      res.json(removed);
-    }
-  });
+	req.post.remove(function(err, removed) {
+		if (err) {
+			next(err);
+		} else {
+			res.json(removed);
+		}
+	});
 };
