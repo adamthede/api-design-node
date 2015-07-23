63e8f59ba4daa0603a93670d88e6c3f7544c1c3d
Adam Thede <adam@thedetech.com>
2015-07-23 15:00:35 -0500

    defining schemas in mongoose

--- server/api/post/postModel.js
+++ server/api/post/postModel.js
@@ -2,7 +2,24 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PostSchema = new Schema({
-
+	title: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	text: {
+		type: String,
+		required: true
+	},
+	author: {
+		type: Schema.Types.ObjectId,
+		ref: 'user',
+		required: true
+	},
+	categories: [{
+		type: Schema.Types.ObjectId,
+		ref: 'category'
+	}]
 });
 
 module.exports = mongoose.model('post', PostSchema);
